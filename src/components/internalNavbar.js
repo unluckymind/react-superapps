f59d324fe7aac781470c7fd05e3e7d53d690e4d5
unluckymind <unluckymind2019@gmail.com>
2019-10-20 16:40:24 +0700

    fixing navbar ui

--- src/components/internalNavbar.js
+++ src/components/internalNavbar.js
@@ -8,6 +8,8 @@ import {
   NavItem,
   NavLink,
   Form,
+  InputGroup,
+  FormGroup,
   Button
 } from "reactstrap";
 
@@ -21,12 +23,12 @@ class InternalNavbar extends Component {
   render() {
     const { humbergerOpen } = this.state;
     return (
-      <Navbar light color="primary">
+      <Navbar light style={navbarStyling.mainComponent}>
         <NavbarBrand href="/" className="mr-auto">
-          Sahabat
+          <img src="../../assets/logo_navbar.svg" height="40px"/>
         </NavbarBrand>
         <Form>
-          <input type="text" placeholder="Search" className="mr-2" />
+        <input placeholder="contoh: batik ..." id="Search" name="Search" type="search" style={navbarStyling.searchbar}/>
         </Form>
         <NavbarToggler
           className="mr-2 ml-2"
@@ -44,6 +46,29 @@ class InternalNavbar extends Component {
         </Collapse>
       </Navbar>
     );
+  }
+}
+
+const navbarStyling = {
+  mainComponent: {
+    background: "#f8f9fa",
+    boxShadow: "0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24)",
+    transition: "all 0.3s cubic-bezier(.25,.8,.25,1)",
+    position: "-webkit-sticky", 
+    position: "sticky", 
+    top: 0, 
+    zIndex:999
+  },
+  searchbar: {
+    width: "84%",
+    borderRadius: "15px",
+    fontSize: "12px",
+    padding: "7px 12px",
+    border: "1px solid #fff",
+    marginRight: "10px",
+    boxShadow: "inset 0 0 2px 2px #ecf0f1",
+    float: "right",
+    outline:0
   }
 }
 
